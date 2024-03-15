3c8d7fa96fade1b69dcff3d08d459c331ed0d15f
TonyKodit <onwulinonso@gmail.com>
2024-03-15 12:55:53 +0000

    Add project files.

---
+++ Bukworm/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bukworm.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
