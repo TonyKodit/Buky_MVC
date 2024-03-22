0dd368993c999c573a38774b474e16998e19f47c
TonyKodit <onwulinonso@gmail.com>
2024-03-22 14:30:57 +0000

    TempData

--- Bukworm/Controllers/CategoryController.cs
+++ Bukworm/Controllers/CategoryController.cs
@@ -34,6 +34,7 @@ namespace Bukworm.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
             return View();
@@ -62,6 +63,8 @@ namespace Bukworm.Controllers
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+
                 return RedirectToAction("Index");
             }
             return View();
@@ -92,6 +95,8 @@ namespace Bukworm.Controllers
             }
             _db.Categories.Remove(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+
             return RedirectToAction("Index");
            
 
