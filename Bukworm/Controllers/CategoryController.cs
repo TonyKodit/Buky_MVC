0742d2aaaa938adb305caa887eeb85bdbad8084e
TonyKodit <onwulinonso@gmail.com>
2024-03-22 10:04:00 +0000

    Custom validation and Asp Validation summary

--- Bukworm/Controllers/CategoryController.cs
+++ Bukworm/Controllers/CategoryController.cs
@@ -25,6 +25,11 @@ namespace Bukworm.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display order cannot exactly match the name");
+            }
+
             if(ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
