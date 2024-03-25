14778c799792b9e8df2ad9885efca9bed40f8251
TonyKodit <onwulinonso@gmail.com>
2024-03-25 12:58:52 +0000

    Seed Product, Product Repository and UnitOf Work

--- Buky.DataAccess/Migrations/20240325105514_AddProductsToDatabase.Designer.cs
+++ Buky.DataAccess/Migrations/20240325105514_AddProductsToDatabase.Designer.cs
@@ -3,6 +3,7 @@ using Buky.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Buky.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240325105514_AddProductsToDatabase")]
+    partial class AddProductsToDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Buky.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using Buky.DataAccess.Data;
+using Buky.DataAccess.Repository.IRepository;
+using Buky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buky.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(Product obj)
+        {
+            _db.Products.Update(obj);
+        }
+    }
+}
