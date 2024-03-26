0203cde410f8b762cb3a12b97e025733cfc78048
TonyKodit <onwulinonso@gmail.com>
2024-03-26 13:24:18 +0000

    Impement ViewModel

--- Bukworm/Areas/Admin/Controllers/ProductController.cs
+++ Bukworm/Areas/Admin/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using Buky.DataAccess.Repository.IRepository;
 using Buky.Models;
+using Buky.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
@@ -24,30 +25,45 @@ namespace Bukworm.Areas.Admin.Controllers
         
         public IActionResult Create()
         {
-            IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category
+            ProductVM productVM = new()
+            {
+                CategoryList = _unitOfWork.Category
                .GetAll().Select(u => new SelectListItem
                {
                    Text = u.Name,
                    Value = u.Id.ToString()
-               });
-            //ViewBag.CategoryList = CategoryList;
-            ViewData["CategoryList"] = CategoryList;
-            return View();
+               }),
+            Product = new Product()
+            };
+            return View(productVM);
         }
 
         [HttpPost]
-        public IActionResult Create(Product obj)
+        public IActionResult Create(ProductVM productVM)
         {
             
 
             if (ModelState.IsValid)
             {
-                _unitOfWork.Product.Add(obj);
+                _unitOfWork.Product.Add(productVM.Product);
                 _unitOfWork.Save();
                 TempData["success"] = "Product created successfully";
                 return RedirectToAction("Index");
             }
-            return View();
+            else
+            {
+
+                productVM.CategoryList = _unitOfWork.Category
+            .GetAll().Select(u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id.ToString()
+            });
+                    
+                return View(productVM);
+            }
+
+            
           
         }
 
