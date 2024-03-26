62b3220a4d2873494cc3f92f68a598e4e3bc440f
TonyKodit <onwulinonso@gmail.com>
2024-03-26 14:39:27 +0000

    Combine Create and Edit pages, Implement Rich Text Editor

--- Bukworm/Areas/Admin/Controllers/ProductController.cs
+++ Bukworm/Areas/Admin/Controllers/ProductController.cs
@@ -23,7 +23,7 @@ namespace Bukworm.Areas.Admin.Controllers
         }
 
         
-        public IActionResult Create()
+        public IActionResult Upsert(int? id)
         {
             ProductVM productVM = new()
             {
@@ -35,13 +35,23 @@ namespace Bukworm.Areas.Admin.Controllers
                }),
             Product = new Product()
             };
-            return View(productVM);
+            if(id == null || id == 0)
+            {
+                //create
+                return View(productVM);
+
+            }
+            else
+            {
+                //update
+                productVM.Product = _unitOfWork.Product.Get(u => u.Id == id);
+                return View(productVM);
+            }
         }
 
         [HttpPost]
-        public IActionResult Create(ProductVM productVM)
+        public IActionResult Upsert(ProductVM productVM, IFormFile? file)
         {
-            
 
             if (ModelState.IsValid)
             {
@@ -67,35 +77,7 @@ namespace Bukworm.Areas.Admin.Controllers
           
         }
 
-        public IActionResult Edit(int? id)
-        {
-            if(id == null || id == 0)
-            {
-                return NotFound();
-            }
-
-            Product productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
-            if (productFromDb == null)
-            {
-                return NotFound();
-            }
-
-            return View(productFromDb);
-        }
-
-        [HttpPost]
-        public IActionResult Edit(Product obj)
-        {
-            if(ModelState.IsValid)
-            {
-                _unitOfWork.Product.Update(obj);
-                _unitOfWork.Save();
-                TempData["success"] = "Product updated successfully";
-                return RedirectToAction("Index");
-            }
-            return View();
-        }
-
+        
         public IActionResult Delete(int? id)
         {
             if(id == null || id == 0)
